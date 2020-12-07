6ab4415db895de33287a9dbfcef6b800df7a040e
yiplee <guoyinl@gmail.com>
2020-12-07 11:57:43 +0800

    deprecated funds & amount on PreOrderReq

--- order.go
+++ order.go
@@ -35,12 +35,24 @@ type Order struct {
 type PreOrderReq struct {
 	PayAssetID  string `json:"pay_asset_id,omitempty"`
 	FillAssetID string `json:"fill_asset_id,omitempty"`
-	// funds 和 amount 二选一
+	// pay amount 和 fill amount 二选一
+	PayAmount  decimal.Decimal `json:"pay_amount,omitempty"`
+	FillAmount decimal.Decimal `json:"fill_amount,omitempty"`
+	// deprecated
 	Funds  decimal.Decimal `json:"funds,omitempty"`
 	Amount decimal.Decimal `json:"amount,omitempty"`
-
 	// deprecated
 	MinAmount decimal.Decimal `json:"min_amount,omitempty"`
+}
+
+func (req *PreOrderReq) fixDeprecated() {
+	if req.Funds.IsPositive() {
+		req.PayAmount = req.Funds
+	}
+
+	if req.Amount.IsPositive() {
+		req.FillAmount = req.Amount
+	}
 }
 
 // PreOrder 预下单
@@ -54,11 +66,20 @@ func PreOrder(ctx context.Context, req *PreOrderReq) (*Order, error) {
 		return nil, err
 	}
 
-	var order *Order
-	if req.Funds.IsPositive() {
-		order, err = Route(pairs, req.PayAssetID, req.FillAssetID, req.Funds)
+	return PreOrderWithPairs(pairs, req)
+}
+
+func PreOrderWithPairs(pairs []*Pair, req *PreOrderReq) (*Order, error) {
+	var (
+		order *Order
+		err   error
+	)
+
+	req.fixDeprecated()
+	if req.PayAmount.IsPositive() {
+		order, err = Route(pairs, req.PayAssetID, req.FillAssetID, req.PayAmount)
 	} else {
-		order, err = ReverseRoute(pairs, req.PayAssetID, req.FillAssetID, req.Amount)
+		order, err = ReverseRoute(pairs, req.PayAssetID, req.FillAssetID, req.FillAmount)
 	}
 
 	if err != nil {
